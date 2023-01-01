c599af1fe52072f9ccff6c784ea747bf2105a3e9
Hermann EHO <hermannleboss@gmail.com>
2023-01-01 17:48:57 +0000

    update content

--- app.vue
+++ app.vue
@@ -1,4 +1,95 @@
 <template>
   <div>
+    <div>
+      Hermann EHO
+    </div>
+
+    <div>
+      <h1>
+        Nice to meet you! I'm Hermann EHO.
+      </h1>
+
+      <p>Based in the UK, I'm a front-end developer passionate about
+        building accessible web apps that users love.</p>
+
+      <a href="#contact">Contact me</a>
+    </div>
+    <div>
+      <div>
+        HTML
+        4 Years Experience
+      </div>
+      <div>
+        CSS
+        4 Years Experience
+      </div>
+      <div>
+        JavaScript
+        4 Years Experience
+      </div>
+      <div>
+        Accessibility
+        4 Years Experience
+      </div>
+      <div>
+        React
+        3 Years Experience
+      </div>
+      <div>
+        Sass
+        3 Years Experience
+      </div>
+    </div>
+
+    <div>
+      <h2>Projects</h2>
+
+      <a href="">Contact me</a>
+    </div>
+    <div>
+      Design portfolio
+      HTML CSS
+      View project
+      View code
+    </div>
+
+    E-learning landing page
+    HTML CSS
+    View project
+    View code
+
+    Todo web app
+    HTML CSS JavaScript
+    View project
+    View code
+
+    Entertainment web app
+    HTML CSS JavaScript
+    View project
+    View code
+
+    Memory Game
+    HTML CSS JavaScript
+    View project
+    View code
+
+    Art gallery showcase
+    HTML CSS JavaScript
+    View project
+    View code
+
+    Contact
+
+    I would love to hear about your project and how I could help. Please
+    fill in the form, and I’ll get back to you as soon as possible.
+
+    Name
+    Email
+    Message
+
+    Send message
+
+    adamkeyes
+
   </div>
 </template>
